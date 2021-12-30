58de88c84fd2138a06798cff3a5436b34913ceb5
abubakarSaddiquee <abubakar.saddiq303@gmail.com>
2021-12-30 15:45:43 +0500

    my first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useContext } from "react";
+import { ThemeContext } from "./contexts/theme";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [{ theme, isDark }, toggleTheme] = useContext(ThemeContext);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="app"
+      style={{ backgroundColor: theme.backgroundColor, color: theme.color }}
+    >
+      <div>
+        <h1>AUTHORATE</h1>
+      </div>
+
+      <button
+        className={isDark ? "btnDark" : "btnLight"}
+        type="button"
+        onClick={toggleTheme}
+      >
+        {isDark ? <i class="fas fa-sun"></i> : <i class="fas fa-moon"></i>}
+      </button>
+      <br />
+
+      {/* <button type="button" onClick={toggleTheme}>
+        Toggle theme
+      </button> */}
+
+      {/* <p>
+        {" "}
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque id
+        neque quis ligula rhoncus ultricies. Suspendisse pretium eros sit amet
+        lectus efficitur, non porta leo pellentesque. Etiam sit amet dictum est.
+        Cras at aliquet turpis. Aliquam vestibulum enim sit amet aliquet
+        blandit. Donec quam odio, accumsan et magna fermentum, tempor volutpat
+        sapien. Pellentesque rutrum, mi eu bibendum bibendum, magna augue
+        pulvinar enim, et ultrices arcu metus sed purus. Fusce in tincidunt
+        nunc. Vivamus purus dui, efficitur fringilla fermentum nec, tristique
+        sed dolor.
+      </p>
+      <p>
+        {" "}
+        Quisque sit amet scelerisque velit. Pellentesque auctor lectus viverra
+        ullamcorper mattis. Phasellus eros ex, suscipit at nunc et, cursus
+        tincidunt arcu. Pellentesque semper id sem vel fringilla. Pellentesque
+        erat nulla, imperdiet a posuere vitae, sagittis sit amet quam.
+        Vestibulum fermentum nisl nec tincidunt gravida. Praesent sed
+        sollicitudin lectus, quis aliquam mi. Vivamus ex magna, dapibus et lorem
+        a, blandit semper orci. Vestibulum eu ante nunc.
+      </p> */}
+      <br />
     </div>
   );
-}
+};
 
 export default App;
