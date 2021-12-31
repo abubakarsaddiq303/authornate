97f9387016f89149509877e5da6310e5d1a860aa
abubakarSaddiquee <abubakar.saddiq303@gmail.com>
2021-12-31 11:21:40 +0500

    second commit

---
+++ src/Main.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+function Main() {
+  return (
+    <div>
+      {" "}
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque id
+        neque quis ligula rhoncus ultricies. Suspendisse pretium eros sit amet
+        lectus efficitur, non porta leo pellentesque. Etiam sit amet dictum est.
+        Cras at aliquet turpis. Aliquam vestibulum enim sit amet aliquet
+        blandit. Donec quam odio, accumsan et magna fermentum, tempor volutpat
+        sapien. Pellentesque rutrum, mi eu bibendum bibendum, magna augue
+        pulvinar enim, et ultrices arcu metus sed purus. Fusce in tincidunt
+        nunc. Vivamus purus dui, efficitur fringilla fermentum nec, tristique
+        sed dolor.
+      </p>
+      <br />
+      <br />
+      <p>
+        {" "}
+        Quisque sit amet scelerisque velit. Pellentesque auctor lectus viverra
+        ullamcorper mattis. Phasellus eros ex, suscipit at nunc et, cursus
+        tincidunt arcu. Pellentesque semper id sem vel fringilla. Pellentesque
+        erat nulla, imperdiet a posuere vitae, sagittis sit amet quam.
+        Vestibulum fermentum nisl nec tincidunt gravida. Praesent sed
+        sollicitudin lectus, quis aliquam mi. Vivamus ex magna, dapibus et lorem
+        a, blandit semper orci. Vestibulum eu ante nunc.
+      </p>
+      <br />
+    </div>
+  );
+}
+
+export default Main;
